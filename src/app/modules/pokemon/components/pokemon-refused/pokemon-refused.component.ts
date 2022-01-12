b9dd8c5eee5b1963def2f8ca66ee0fc019596aa5
Greta9626 <86754253+Greta9626@users.noreply.github.com>
2022-01-12 13:17:49 +0100

    upload exercise

---
+++ src/app/modules/pokemon/components/pokemon-refused/pokemon-refused.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-refused',
+  templateUrl: './pokemon-refused.component.html',
+  styleUrls: ['./pokemon-refused.component.scss']
+})
+export class PokemonRefusedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
